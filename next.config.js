a94ae87daafc61b95c9f216e86342e03e44afec5
Matthew Wagerfield <matthew@wagerfield.com>
2021-05-16 15:48:03 +0100

    chore: initial commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  i18next: {
+    title: "Testing 123"
+  }
+}
---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+import { readFileSync } from "fs"
+
+export const getStaticProps = () => {
+  const { i18next } = require("../next.config")
+  const config = readFileSync("next.config.js", "utf-8")
+
+  return {
+    props: {
+      title: i18next.title,
+      config
+    }
+  }
+}
+
+export const IndexPage = ({ title, config }) => (
+  <main>
+    <h1>{title}</h1>
+    <pre>{config}</pre>
+  </main>
+)
+
+export default IndexPage
